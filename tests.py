24b76fcf8a74f8958a7c352e2d5087b2edc2927e
Flávio Teixeira Sales <flaviotsales@gmail.com>
2019-02-18 22:26:06 -0300

    Métodos de teste renomeados com nome correto da função

--- tests.py
+++ tests.py
@@ -2,10 +2,10 @@ import unittest
 import diarios
 
 class TseTests(unittest.TestCase):
-    def test_hashDiarioPorData_diarioExisteNaData_retornaHash(self):
+    def test_getDiarioMd5PorData_diarioExisteNaData_retornaHash(self):
         self.assertEqual(diarios.getDiarioMd5PorData('2018-12-02'), '78addcf872a1fcfbb10384da4b8ba696')
 
-    def test_hashDiarioPorData_diarioNaoExisteNaData_retornaNone(self):
+    def test_getDiarioMd5PorData_diarioNaoExisteNaData_retornaNone(self):
         self.assertEqual(diarios.getDiarioMd5PorData('2018-10-02'), None)
 
 unittest.main()
