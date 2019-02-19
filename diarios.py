270a09bbd0a6c9644eb9e874f48ae1133415258f
Flávio Teixeira Sales <flaviotsales@gmail.com>
2019-02-18 23:30:36 -0300

    Implementada função baixaDiarioPorId

--- diarios.py
+++ diarios.py
@@ -28,5 +28,27 @@ def getIdDiarioPorData(data: str):
         id = matches[0].replace('chamarCaptcha(', '')
         return id
 
+def __getFileNameFromHeader(headers):
+    return headers['Content-Disposition']\
+        .replace('attachment;filename="', '')\
+        .replace('"', '')\
+        .replace('/', '_')
+
 def baixaDiarioPorId(id, nomeArquivo = None):
-    pass
+    url = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=downloadDiario'
+    response = requests.post(
+        url,
+        {
+            'captchaValidacao': 'ok',
+            'id': id,
+            'tribunal': 'TSE'
+        }
+    )
+
+    if (response.status_code == 200 and response.headers['Content-Type'] == 'application/octet-stream'):
+        response.headers['Content-Type']
+        if nomeArquivo == None:
+            nomeArquivo = __getFileNameFromHeader(response.headers)
+
+        with open(nomeArquivo, 'wb') as f:
+            f.write(response.content)
