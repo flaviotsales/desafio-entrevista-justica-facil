4568c3a6195c957bf59296f89a0080a7712f915d
Flávio Teixeira Sales <flaviotsales@gmail.com>
2019-02-18 23:26:19 -0300

    Corrigidas datas dos testes

--- tests.py
+++ tests.py
@@ -4,16 +4,16 @@ import os
 
 class TseTests(unittest.TestCase):
     def test_getDiarioMd5PorData_diarioExisteNaData_retornaHash(self):
-        self.assertEqual(diarios.getDiarioMd5PorData('12/02/2018'), '78addcf872a1fcfbb10384da4b8ba696')
+        self.assertEqual(diarios.getDiarioMd5PorData('12/02/2019'), '78addcf872a1fcfbb10384da4b8ba696')
 
     def test_getDiarioMd5PorData_diarioNaoExisteNaData_retornaNone(self):
-        self.assertEqual(diarios.getDiarioMd5PorData('10/02/2018'), None)
+        self.assertEqual(diarios.getDiarioMd5PorData('10/02/2019'), None)
 
     def test_getIdDiarioPorData_diarioExisteNaData_retornaId(self):
-        self.assertEqual(diarios.getIdDiarioPorData('12/02/2018'), '96642')
+        self.assertEqual(diarios.getIdDiarioPorData('12/02/2019'), '96642')
 
     def test_getIdDiarioPorData_diarioNaoExisteNaData_retornaNone(self):
-        self.assertEqual(diarios.getIdDiarioPorData('10/02/2018'), None)
+        self.assertEqual(diarios.getIdDiarioPorData('10/02/2019'), None)
 
     def test_baixaDiarioPorId_diarioExiste_arquivoSalvoComNomePadrao(self):
         diarios.baixaDiarioPorId('96642')
