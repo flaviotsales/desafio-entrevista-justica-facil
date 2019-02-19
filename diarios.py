afedf42298f3c5d7d471dd288ae856aaab3d63a9
Flávio Teixeira Sales <flaviotsales@gmail.com>
2019-02-18 22:58:38 -0300

    Implementada função getDiarioPorData

--- diarios.py
+++ diarios.py
@@ -1,5 +1,29 @@
+import requests
+import re
+
 def getDiarioMd5PorData(data: str):
     pass
 
 def getIdDiarioPorData(data: str):
-    pass
+    url = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diarioTxt.do'
+
+    response = requests.post(
+        url,
+        {
+            'action': 'buscarDiarios',
+            'voDiarioSearch.tribunal': 'TSE',
+            'page': 'diarioPageTextualList.jsp',
+            'voDiarioSearch.dataPubIni': data,
+            'voDiarioSearch.dataPubFim': data
+        }
+    )
+    
+    html = str(response.content)
+
+    p = re.compile('chamarCaptcha\(\d+')
+    matches = p.findall(html)
+    if len(matches) == 0:
+        return None
+    else:
+        id = matches[0].replace('chamarCaptcha(', '')
+        return id
