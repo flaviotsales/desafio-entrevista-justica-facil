08b202e511a2712a42e6629679c1ab2a98b9efa4
Flávio Teixeira Sales <flaviotsales@gmail.com>
2019-02-18 22:57:17 -0300

    Alterado formato da data para brasileira

--- tests.py
+++ tests.py
@@ -3,15 +3,15 @@ import diarios
 
 class TseTests(unittest.TestCase):
     def test_getDiarioMd5PorData_diarioExisteNaData_retornaHash(self):
-        self.assertEqual(diarios.getDiarioMd5PorData('2018-12-02'), '78addcf872a1fcfbb10384da4b8ba696')
+        self.assertEqual(diarios.getDiarioMd5PorData('12/02/2018'), '78addcf872a1fcfbb10384da4b8ba696')
 
     def test_getDiarioMd5PorData_diarioNaoExisteNaData_retornaNone(self):
-        self.assertEqual(diarios.getDiarioMd5PorData('2018-10-02'), None)
+        self.assertEqual(diarios.getDiarioMd5PorData('10/02/2018'), None)
 
     def test_getIdDiarioPorData_diarioExisteNaData_retornaHash(self):
-        self.assertEqual(diarios.getIdDiarioPorData('2018-12-02'), '96642')
+        self.assertEqual(diarios.getIdDiarioPorData('12/02/2018'), '96642')
 
     def test_getIdDiarioPorData_diarioNaoExisteNaData_retornaNone(self):
-        self.assertEqual(diarios.getIdDiarioPorData('2018-10-02'), None)
+        self.assertEqual(diarios.getIdDiarioPorData('10/02/2018'), None)
 
 unittest.main()
